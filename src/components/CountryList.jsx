493b1de32f5db28127a46aad28c18621adf465ea
Doug Sellers <DouglasSSellers@gmail.com>
2025-01-30 19:23:30 -0500

    Initial commit - added project files

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import CountryItem from './CountryItem';
+import Message from './Message';
+import Spinner from './Spinner';
+
+import styles from './CountryList.module.css';
+import { useCities } from '../contexts/CitiesContext';
+
+function CountryList() {
+  const { cities, isLoading } = useCities();
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first country by clicking on the first country in the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    } else {
+      return arr;
+    }
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem key={country.id} country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
